8719d0f045a122ab11688813a868682ff78ffb39
dschwertfeger <david.schwertfeger@googlemail.com>
2013-07-29 12:50:41 +0200

    Updated LibraryView to work with keywords and the datepicker.

--- site/js/views/library.js
+++ site/js/views/library.js
@@ -39,10 +39,21 @@ app.LibraryView = Backbone.View.extend({
 
         $( '#addBook div' ).children( 'input' ).each( function( i, el ) {
             if( $( el ).val() !== '' ) {
-                formData[ el.id ] = $( el ).val();
+                if ( el.id === 'keywords' ) {
+                    formData[ el.id ] = [];
+                    _.each( $( el ).val().split( ' ' ), function( keyword ) {
+                        formData[ el.id ].push({ 'keyword': keyword});
+                    });
+                } else if( el.id === 'releaseDate' ) {
+                    formData[ el.id ] = $( '#releaseDate' ).datepicker( 'getDate').getTime();
+                } else {
+                    formData[ el.id ] = $( el ).val();
+                }
             }
+            // Clear input field value
+            $( el ).val('');
         });
 
-        this.collection.add( new app.Book( formData ) );
+        this.collection.create( formData );
     }
 });
